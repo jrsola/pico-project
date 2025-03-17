8b7dc932ef0e90af73d3af253519eddfe1866010
Dionichi <manel.calvet@gmail.com>
2025-03-17 20:40:14 +0100

    test bootsel button

--- main.cpp
+++ main.cpp
@@ -17,6 +17,12 @@
 #include "rgbled.hpp"
 #include "button.hpp"
 
+#include <hardware/sync.h>
+#include "hardware/structs/ioqspi.h"
+#include "hardware/structs/sio.h"
+
+// bool __no_inline_not_in_flash_func(get_bootsel_button)();
+
 using namespace pimoroni;
 
 class Color {
@@ -358,6 +364,45 @@ std::string get_time() {
     return std::string(buffer);
 } 
 
+
+//get_bootsel_button()
+
+bool __no_inline_not_in_flash_func(get_bootsel_button)() {
+    const uint CS_PIN_INDEX = 1;
+
+    // Must disable interrupts, as interrupt handlers may be in flash, and we
+    // are about to temporarily disable flash access!
+    uint32_t flags = save_and_disable_interrupts();
+
+    // Set chip select to Hi-Z
+    hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
+                    GPIO_OVERRIDE_LOW << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
+                    IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
+
+    // Note we can't call into any sleep functions in flash right now
+    for (volatile int i = 0; i < 1000; ++i);
+
+    // The HI GPIO registers in SIO can observe and control the 6 QSPI pins.
+    // Note the button pulls the pin *low* when pressed.
+#if PICO_RP2040
+    #define CS_BIT (1u << 1)
+#else
+    #define CS_BIT SIO_GPIO_HI_IN_QSPI_CSN_BITS
+#endif
+    bool button_state = !(sio_hw->gpio_hi_in & CS_BIT);
+
+    // Need to restore the state of chip select, else we are going to have a
+    // bad time when we return to code in flash!
+    hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
+                    GPIO_OVERRIDE_NORMAL << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
+                    IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
+
+    restore_interrupts(flags);
+
+    return button_state;
+}
+
+
 int main() {
 
     // Initialize Pico
@@ -381,6 +426,8 @@ int main() {
 
     screen.writeln("TIME IS: ", "yellow");
 
+ 
+
     std::string time_string;
     led.set_rgb("magenta");
     while(true) {
@@ -395,6 +442,10 @@ int main() {
         if(button_a.raw() && button_y.raw()) {
             reset_usb_boot(0, 0);
         } 
+
+        if(get_bootsel_button()) {
+            led.new_blink(5,500,"green");
+        }
 
         led.blink_update();
         screen.writexy(150,150,time_string, "dark blue");
