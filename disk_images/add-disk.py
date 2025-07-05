f304a08d1aebffbf7cc036d2195119b1ffa5086f
jrsola <jrsola@gmail.com>
2025-07-04 22:24:02 -0700

    habemus FAT al final de la flash, pero no ens agrada

--- disk_images/add-disk.py
+++ disk_images/add-disk.py
@@ -1,37 +1,50 @@
 #!/usr/bin/env python3
 
-FLASH_SIZE = 2 * 1024 * 1024         # 2 MB
-DISK_SIZE = 128 * 1024               # 128 KB
-DISK_OFFSET = FLASH_SIZE - DISK_SIZE
+import sys
+import os
 
-INPUT_BIN = "build/pico-project.bin"
-DISK_IMG = "disk_images/fat12_disk_128.img"
-OUTPUT_BIN = "build/pico-project+disk.bin"
+# Pico Flash size = 2MB
+FLASH_SIZE = 2 * 1024 * 1024
 
 def main():
-    with open(INPUT_BIN, "rb") as f:
+    if len(sys.argv) != 4:
+        print(f"Incorrect number of arguments: {len(sys.argv)}")
+        print(f"Syntax: {sys.argv[0]} <input_binary> <disk_image> <output_binary")
+        sys.exit(1)
+
+    input_binary = f"{sys.argv[1]}"
+    disk_image = os.path.join(os.path.dirname(os.path.abspath(__file__)), sys.argv[2])
+    output_binary = f"{sys.argv[3]}"
+
+    # Read compiled (uncompressed) .bin file in a read/write byte array
+    with open(input_binary, "rb") as f:
         firmware = bytearray(f.read())
 
-    with open(DISK_IMG, "rb") as f:
-        disk = f.read()
-
-    if len(disk) != DISK_SIZE:
-        raise ValueError("La imatge FAT12 no fa exactament 128 KB")
-
-    # Assegura que el binari fa exactament 2MB
+    # Check size of input binary (needs to be exactly 2MB)
     if len(firmware) < FLASH_SIZE:
         firmware += b'\x00' * (FLASH_SIZE - len(firmware))
     elif len(firmware) > FLASH_SIZE:
-        raise ValueError("El firmware excedeix els 2 MB de la flash")
+        raise ValueError("Error: Binary image beyond Pico Flash Spaice")
 
-    # Substitueix els últims 128 KB amb la imatge FAT12
-    firmware[DISK_OFFSET:] = disk
+    # Read disk image file in a read only byte array
+    with open(disk_image, "rb") as f:
+        disk = bytes(f.read())
 
-    # Escriu el nou fitxer
-    with open(OUTPUT_BIN, "wb") as f:
+    disk_size = len(disk)
+
+    if disk_size > FLASH_SIZE:
+        raise ValueError("Disk image is bigger than flash space!")
+    
+    disk_offset = FLASH_SIZE - disk_size
+    
+    # Overwrite the disk image on the flash memory from the end
+    firmware[disk_offset:] = disk
+
+    # Write new binary file: code + disk image 
+    with open(output_binary, "wb") as f:
         f.write(firmware)
 
-    print(f"[OK] Generat {OUTPUT_BIN} amb la imatge FAT12 incrustada als últims 128 KB")
+    print(f"[OK] {output_binary} generated successfully {disk_size} bytes")
 
 if __name__ == "__main__":
     main()
